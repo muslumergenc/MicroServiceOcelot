cf3bca4da427d5aa967cc8c4bd927a2efb142552
Müslüm Ergenç <muslumergenc@outlook.com.tr>
2024-10-19 11:28:31 +0300

    Add project files.

---
+++ Services/RestAPI/Reservation.API.Models/ReservationDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Reservation.API.Models
+{
+    public class ReservationDTO
+    {
+        public int Id { get; set; }
+        public int BookingNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut{ get; set; }
+        public DateTime BookingDate { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Services/RestAPI/Reservation.API.Services/ReservationService.cs
@@ -0,0 +1,21 @@
+﻿using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+
+namespace Reservation.API.Services
+{
+    public class ReservationService : IReservationService
+    {
+        public ReservationDTO GetResByBookingNumber(int number)
+        {
+            return new ReservationDTO
+            {
+                Id = (new Random()).Next(100),
+                Amount = (new Random()).Next(10000),
+                BookingDate = DateTime.Now,
+                CheckIn = DateTime.Now.Date.AddDays(30),
+                CheckOut = DateTime.Now.Date.AddDays(33),
+                BookingNumber = number
+            };
+        }
+    }
+}
